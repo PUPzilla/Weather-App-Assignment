f6050ee7b1c85b6adc1195155140f9252199f5f9
PUPzilla <W0491405@campus.nscc.ca>
2025-09-17 18:28:23 -0300

    Messages list working

--- app/src/main/java/com/example/gavmacdonald_weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/gavmacdonald_weatherapp/MainActivity.kt
@@ -1,87 +1,129 @@
 package com.example.gavmacdonald_weatherapp
 
+import SampleData
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.enableEdgeToEdge
+import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import com.example.gavmacdonald_weatherapp.ui.theme.GavMacDonaldWeatherAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        setContent {
+            GavMacDonaldWeatherAppTheme {
+                Conversation(SampleData.conversationSample)
+            }
+        }
     }
 }
 
-@Preview(showBackground = true)
+data class Message(val author: String, val body: String)
+
 @Composable
-fun Greeting() {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
-        modifier = Modifier.fillMaxSize()
-    ) {
-        LocationImage(
-            message = "Hali"
+fun MessageCard(msg: Message) {
+    Row(modifier = Modifier.padding(all = 8.dp)){
+        Image(
+            painter = painterResource(R.drawable.ic_launcher_foreground),
+            contentDescription = null,
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
         )
-    }
-}
+        Spacer(modifier = Modifier.width(8.dp))
 
-@Composable
-fun GreetingText(message: String, modifier: Modifier = Modifier) {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
-        modifier = Modifier.fillMaxSize()
-    ) {
-        val offset = Offset(5.0f, 10.0f)
-        Text(
-            text = message,
-            style = TextStyle(
-                fontSize = 48.sp,
-                color = Color.Magenta,
-                shadow = Shadow(
-                    color = Color.Cyan, offset = offset, blurRadius = 3f
-                )
+        var isExpanded by remember { mutableStateOf(false) }
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface,
+        )
+
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+            Text(
+                text = msg.author,
+                color = Color.White,
+                style = MaterialTheme.typography.titleSmall
             )
-        )
+
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                shadowElevation = 1.dp,
+                color = surfaceColor,
+                modifier = Modifier.animateContentSize().padding(1.dp)
+            ) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+        }
     }
 }
 
 @Composable
-fun LocationImage(message: String, modifier: Modifier = Modifier) {
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message)
+        }
+    }
+}
+
+@Preview(showBackground = false)
+@Composable
+fun PreviewConversation() {
+    GavMacDonaldWeatherAppTheme {
+        LocationImage()
+        Conversation(SampleData.conversationSample)
+    }
+}
+
+@Preview
+@Composable
+fun LocationImage() {
     val image = painterResource(R.drawable.location)
-    Box(modifier) {
+    Box {
         Image(
             painter = image,
             contentDescription = null,
             contentScale = ContentScale.Crop,
-            colorFilter = ColorFilter.tint(Color.DarkGray, BlendMode.Hardlight)
-        )
-        GreetingText(
-            message = message,
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(8.dp)
+            colorFilter = ColorFilter.tint(Color.hsv(240.0F, 0.35F, 0.5F), BlendMode.Exclusion)
         )
     }
 }
